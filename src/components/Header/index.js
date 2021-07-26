09ac0696d172fdd11d1e7df079b398c8942dfa0f
ANB47\Monitora <gabrielrspupo@gmail.com>
2021-07-26 01:13:17 -0300

    Add accessibility modal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { Container, Nav, Navbar, Button, Form, FormControl } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Container, Nav, Navbar, Button, Form, FormControl, CloseButton } from 'react-bootstrap';
 import { Link, withRouter } from 'react-router-dom';
 import BrejasContext from '../../BrejasContext';
 import './styles.css';
@@ -12,6 +12,7 @@ function Header({history}) {
     const accessibiltyIcon = require('../../assets/accessibility.svg').default;
     const dropdownArrow = require('../../assets/dropdown.svg').default;
     const context = useContext(BrejasContext);
+    const [modalOpen, setModalOpen] = useState(false);
 
     const validateLoggedUser = () => {
         if (context.loggedUser) history.push('/checkout');
@@ -19,37 +20,49 @@ function Header({history}) {
     }
 
     return (
-        <Navbar>
-            <Container>
-            <Navbar.Brand><Link to="/">Brejas.com</Link></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="justify-content-end"  style={{ width: "100%" }}>
-                <Form inline="true" className="navbar-search-bar">
-                    <FormControl type="text" placeholder="Faça sua pesquisa" className="mr-sm-2" />
-                </Form>
-                {context.loggedUser ? 
-                    <div className="navbar-logged-user" onClick={() => {context.setLoggedUser(false)}}>
-                        <img className="navbar-logged-user-avatar" src={Avatar} alt="Avatar" />
-                        <div>
-                            João
-                            <img className="navbar-logged-user-dropdown" src={dropdownArrow} alt="Opções da conta" />
+        <>
+            <Navbar>
+                <Container>
+                <Navbar.Brand><Link to="/">Brejas.com</Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="justify-content-end"  style={{ width: "100%" }}>
+                    <Form inline="true" className="navbar-search-bar">
+                        <FormControl type="text" placeholder="Faça sua pesquisa" className="mr-sm-2" />
+                    </Form>
+                    {context.loggedUser ? 
+                        <div className="navbar-logged-user" onClick={() => {context.setLoggedUser(false)}}>
+                            <img className="navbar-logged-user-avatar" src={Avatar} alt="Avatar" />
+                            <div>
+                                João
+                                <img className="navbar-logged-user-dropdown" src={dropdownArrow} alt="Opções da conta" />
+                            </div>
                         </div>
+                        :
+                        <Button className="navbar-login-btn" onClick={() => history.push('/login')}>
+                            Entrar<img className="navbar-login-icon" src={notLoggedIcon} alt='Não logado'/>
+                        </Button>
+                    }
+                    <div className='navbar-shopping-cart'>
+                    <Nav.Link onClick={validateLoggedUser}><img className="navbar-shopping-cart-icon" src={shoppingCartIcon} alt='Carrinho'/></Nav.Link>
+                    {context.itemsOnCart > 0 && <div className="navbar-shopping-cart-indicator">1</div>}
                     </div>
-                    :
-                    <Button className="navbar-login-btn" onClick={() => history.push('/login')}>
-                        Entrar<img className="navbar-login-icon" src={notLoggedIcon} alt='Não logado'/>
-                    </Button>
-                }
-                <div className='navbar-shopping-cart'>
-                <Nav.Link onClick={validateLoggedUser}><img className="navbar-shopping-cart-icon" src={shoppingCartIcon} alt='Carrinho'/></Nav.Link>
-                {context.itemsOnCart > 0 && <div className="navbar-shopping-cart-indicator">1</div>}
+                    <Nav.Link onClick={() => setModalOpen(true)}><img className="navbar-accessibility-icon" src={accessibiltyIcon} alt='Acessibilidade'/></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <dialog open={modalOpen} className="accessibility-modal">
+                <div className="accessibility-modal-content">
+                    <Button>Ativar auto contraste</Button>
+                    <div className="font-size-controllers">
+                        <Button>a</Button>
+                        <Button>A</Button>
+                    </div>
+                    <CloseButton onClick={() => setModalOpen(false)} className="accessibilty-modal-close"/>
                 </div>
-                <Nav.Link href="#home"><img className="navbar-accessibility-icon" src={accessibiltyIcon} alt='Acessibilidade'/></Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-            </Container>
-        </Navbar>
+            </dialog>
+        </>
     );
 }
 
